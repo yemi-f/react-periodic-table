8ca18b15695800d02e759c3595dc8cce05036da9
Yemi Fakorede <yemifakorede@gmail.com>
2020-07-08 11:19:37 -0400

    Updates

---
+++ precache-manifest.694e7f44800f49b0382336fbe09539cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22a2caa41eeb11a64cb8a114500c0651",
+    "url": "/react-periodic-table/index.html"
+  },
+  {
+    "revision": "640e3bacffb3660a6789",
+    "url": "/react-periodic-table/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "ed97e16c9ea7aa58622f",
+    "url": "/react-periodic-table/static/css/main.8f2344c4.chunk.css"
+  },
+  {
+    "revision": "640e3bacffb3660a6789",
+    "url": "/react-periodic-table/static/js/2.4342b206.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-periodic-table/static/js/2.4342b206.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed97e16c9ea7aa58622f",
+    "url": "/react-periodic-table/static/js/main.ba24bda3.chunk.js"
+  },
+  {
+    "revision": "2cb10be658c282ca7710",
+    "url": "/react-periodic-table/static/js/runtime-main.3bf7aa4f.js"
+  }
+]);
